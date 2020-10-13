49c66c1ae24f0ad51b73ee0bb21cc6aa2396599d
brayvasq <brayvasq@gmail.com>
2020-10-13 15:44:38 -0500

    change: name of some campaign methods

--- lib/clever_tap/campaign.rb
+++ lib/clever_tap/campaign.rb
@@ -27,6 +27,7 @@ class CleverTap
     ALLOWED_IDENTITIES = %w[FBID Email Identity objectId GPID ID].freeze
     TO_STRING = 'to'.freeze
     TAG_GROUP = 'tag_group'.freeze
+    CAMPAIGN_ID = 'campaign_id'.freeze
     CONTENT = 'content'.freeze
     PROVIDER_NICK_NAME = 'provider_nick_name'.freeze
     NOTIFICATION_SENT = 'notification_sent'.freeze
@@ -38,35 +39,47 @@ class CleverTap
     PLATFORM_SPECIFIC = 'platform_specific'.freeze
     MAX_USERS_PER_CAMPAIGN = 1000
 
-    def initialize(**args)
-      @to = args[:to]
-      @tag_group = args[:tag_group]
-      @content = args[:content]
-      @provider_nick_name_pair = args[:provider_nick_name_pair]
-      @notification_sent = args[:notification_sent]
-      @respect_frequency_caps = args[:respect_frequency_caps]
-      @wzrk_cid = args[:wzrk_cid]
-      @badge_id = args[:badge_id]
-      @badge_icon = args[:badge_icon]
-      @mutable_content = args[:mutable_content]
-      @platform_specific = args[:platform_specific]
+    def initialize(to: nil,
+                   content: nil,
+                   tag_group: nil,
+                   campaign_id: nil,
+                   provider_nick_name: nil,
+                   notification_sent: nil,
+                   respect_frequency_caps: nil,
+                   wzrk_cid: nil,
+                   badge_id: nil,
+                   badge_icon: nil,
+                   mutable_content: nil,
+                   platform_specific: nil)
+      @to = to
+      @tag_group = tag_group
+      @campaign_id = campaign_id
+      @content = content
+      @provider_nick_name = provider_nick_name
+      @notification_sent = notification_sent
+      @respect_frequency_caps = respect_frequency_caps
+      @wzrk_cid = wzrk_cid
+      @badge_id = badge_id
+      @badge_icon = badge_icon
+      @mutable_content = mutable_content
+      @platform_specific = platform_specific
     end
 
     def to_h
-      put_to_pair
-        .merge(put_tag_group_pair)
-        .merge(put_content_pair)
-        .merge(put_provider_nick_name_pair)
-        .merge(put_notification_sent_pair)
-        .merge(put_respect_frequency_caps_pair)
-        .merge(put_wzrk_cid_pair)
-        .merge(put_badge_id_pair)
-        .merge(put_badge_icon_pair)
-        .merge(put_mutable_content_pair)
-        .merge(put_platform_specific_pair)
+      to_hash
+        .merge(tag_group_hash)
+        .merge(content_hash)
+        .merge(provider_nick_name_hash)
+        .merge(notification_sent_hash)
+        .merge(respect_frequency_caps_hash)
+        .merge(wzrk_cid_hash)
+        .merge(badge_id_hash)
+        .merge(badge_icon_hash)
+        .merge(mutable_content_hash)
+        .merge(platform_specific_hash)
     end
 
-    def put_to_pair
+    def to_hash
       raise NoReceiversError if @to.to_h.empty?
       raise InvalidIdentityTypeError unless allowed?(@to.keys)
       raise NoReceiversError if @to.values.all?(&:empty?)
@@ -75,54 +88,59 @@ class CleverTap
       { TO_STRING => @to }
     end
 
-    def put_tag_group_pair
+    def tag_group_hash
       return {} unless @tag_group
       { TAG_GROUP => @tag_group }
     end
 
-    def put_content_pair
+    def campaign_id_hash
+      return {} unless @campaign_id
+      { CAMPAIGN_ID => @campaign_id}
+    end
+
+    def content_hash
       raise NoContentError if @content.to_h.empty?
       raise NoContentError if @content.to_h['body'].nil?
 
       { CONTENT => @content }
     end
 
-    def put_provider_nick_name_pair
-      return {} unless @provider_nick_name_pair
-      { PROVIDER_NICK_NAME => @provider_nick_name_pair }
+    def provider_nick_name_hash
+      return {} unless @provider_nick_name
+      { PROVIDER_NICK_NAME => @provider_nick_name }
     end
 
-    def put_notification_sent_pair
+    def notification_sent_hash
       return {} unless @notification_sent
       { NOTIFICATION_SENT => @notification_sent }
     end
 
-    def put_respect_frequency_caps_pair
+    def respect_frequency_caps_hash
       return {} if @respect_frequency_caps.nil?
       { RESPECT_FREQUENCY_CAPS => @respect_frequency_caps }
     end
 
-    def put_wzrk_cid_pair
+    def wzrk_cid_hash
       return {} unless @wzrk_cid
       { WZRK_CID => @wzrk_cid }
     end
 
-    def put_badge_id_pair
+    def badge_id_hash
       return {} unless @badge_id
       { BADGE_ID => @badge_id }
     end
 
-    def put_badge_icon_pair
+    def badge_icon_hash
       return {} unless @badge_icon
       { BADGE_ICON => @badge_icon }
     end
 
-    def put_mutable_content_pair
+    def mutable_content_hash
       return {} if @mutable_content.nil?
       { MUTABLE_CONTENT => @mutable_content }
     end
 
-    def put_platform_specific_pair
+    def platform_specific_hash
       return {} unless @platform_specific
       { PLATFORM_SPECIFIC => @platform_specific }
     end
